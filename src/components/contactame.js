3c9a50591b1e061cceb30e80d1f4872651faa110
aris-osorio <aris.osorio@alumnos.udg.mx>
2021-01-13 00:35:40 -0600

    contactame base ok

--- src/components/contactame.js
+++ src/components/contactame.js
@@ -1,10 +1,36 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedinIn, faGithub } from '@fortawesome/free-brands-svg-icons'
+
 export default function Contactame() {
     return (
-        <section id = "contactame">
-            <div className="container">
-                <div className="vh-100 d-flex justify-content-center align-items-center">
-                    <div>
+        <section id="contactame">
+            <div className="bg-black">
+                <div className="vh-100 p-5">
+                    <div className="col-12 d-flex justify-content-center p-3">
                         <h1>Contactame</h1>
+                    </div>
+                    <div className="row d-flex m-0">
+                        <div className="col-sm-6 mb-3">
+                            <label for="inputName">Tu nombre</label>
+                            <input id="inputName" className="form-control form-control-lg" type="text" placeholder="Ingresa tu nombre aqui" />
+                        </div>
+                        <div className="col-sm-6 mb-3">
+                            <label for="inputEmail">Tu correo</label>
+                            <input type="email" className="form-control form-control-lg" id="inputEmail" placeholder="name@example.com" />
+                        </div>
+                    </div>
+                    <div className="p-3">
+                        <label for="inputMensaje">Tu mensaje</label>
+                        <textarea className="form-control  mb-3" id="inputMensaje" rows="3" />
+                        <button type="button" className="btn btn-primary">Enviar</button>
+                    </div>
+                    <div className="col-12 d-flex justify-content-center p-2">
+                        <div className="p-4">
+                            <FontAwesomeIcon icon={faLinkedinIn} className="fa-4x" />
+                        </div>
+                        <div className="p-4">
+                            <FontAwesomeIcon icon={faGithub} className="fa-4x" />
+                        </div>
                     </div>
                 </div>
             </div>
--- src/components/proyectos.js
+++ src/components/proyectos.js
@@ -1,7 +1,7 @@
 export default function Proyectos() {
     return (
         <section id = "proyectos">
-            <div className="container">
+            <div className="bg-black">
                 <div className="vh-100 d-flex justify-content-center align-items-center">
                     <div>
                         <h1>Proyectos</h1>
